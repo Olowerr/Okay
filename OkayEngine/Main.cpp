bf3d4887fe6744ccc4a7c5c5e9f44e0c66439843
Olowerr <oliver.klingspor@hotmail.com>
2022-07-10 01:19:06 +0200

    i

--- OkayEngine/Main.cpp
+++ OkayEngine/Main.cpp
@@ -1,12 +1,7 @@
 #include "System.h"
 
-//#include "Importer.h"
-
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	//print();
-	//foo();
-
 	System system;
 
 	if (!system.Initiate())
