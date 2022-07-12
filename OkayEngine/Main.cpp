a6fcabd2e52fbf748be58860931e0447bfb0b65a
Olowerr <oliver.klingspor@hotmail.com>
2022-07-12 16:43:46 +0200

    Included EnTT

--- OkayEngine/Main.cpp
+++ OkayEngine/Main.cpp
@@ -2,8 +2,6 @@
 
 #include "System.h"
 
-#include "ResourceManager.h"
-
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
