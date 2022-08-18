1196822a2c241093f214bdceb86e7bf42dda7b09
Olowerr <oliver.klingspor@hotmail.com>
2022-08-18 20:35:11 +0200

    assimp animation testing

--- OkayEngine/Main.cpp
+++ OkayEngine/Main.cpp
@@ -13,6 +13,36 @@
 void RedirectIOToConsole();
 #endif
 
+struct TimeStamp
+{
+    float time;
+    aiVector3t<float> pos;
+    aiQuaterniont<float> rot;
+    aiVector3t<float> scale;
+};
+
+struct Joint
+{
+    std::string parentName;
+    aiMatrix4x4 bindPose;
+    std::vector<TimeStamp> stamps;
+};
+
+void FindParentName(std::map<std::string, Joint>& joints, aiNode* node)
+{
+    if (joints.find(node->mName.C_Str()) != joints.end())
+    {
+        // skip scaling->rotation->preRotation->translation
+        std::string_view parentJointName = node->mParent->mParent->mParent->mParent->mParent->mName.C_Str();
+
+        if (parentJointName != "RootNode")
+            joints[node->mName.C_Str()].parentName = parentJointName;
+    }
+
+    for (UINT i = 0; i < node->mNumChildren; i++)
+        FindParentName(joints, node->mChildren[i]);
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -48,15 +78,50 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     memcpy(verts.data(), mesh->mVertices, sizeof(aiVector3t<float>) * verts.size());
 
     std::vector< aiVector3t<float>> weightVerts(weights[2].size());
-
-    std::vector<aiNodeAnim> nodes(ani->mNumChannels);
-    for (size_t i = 0; i < nodes.size(); i++)
-        nodes[i] = *ani->mChannels[i];
-    
-
     for (size_t i = 0; i < weights[2].size(); i++)
         weightVerts[i] = verts[weights[1][i].mVertexId];
-    
+
+    std::vector<aiNodeAnim> aniChannels(ani->mNumChannels);
+    for (size_t i = 0; i < aniChannels.size(); i++)
+        aniChannels[i] = *ani->mChannels[i];
+ 
+    std::map<std::string, Joint> joints;
+    for (UINT i = 0; i < mesh->mNumBones; i++)
+    {
+        auto& traChannel = *ani->mChannels[i * 3];
+        auto& rotChannel = *ani->mChannels[i * 3 + 1];
+        auto& scaChannel = *ani->mChannels[i * 3 + 2];
+
+        Joint& joint = joints[mesh->mBones[i]->mName.C_Str()];
+
+        joint.bindPose = mesh->mBones[i]->mOffsetMatrix;
+
+        joint.stamps.resize(traChannel.mNumPositionKeys);
+        for (size_t i = 0; i < joint.stamps.size(); i++)
+        {
+            joint.stamps[i].time = (float)traChannel.mPositionKeys[i].mTime;
+
+            joint.stamps[i].pos = traChannel.mPositionKeys[i].mValue;
+            joint.stamps[i].rot = rotChannel.mRotationKeys[i].mValue;
+            joint.stamps[i].scale = scaChannel.mScalingKeys[i].mValue;
+        }
+    }
+
+    FindParentName(joints, pScene->mRootNode);
+
+    //for (aiNode* node : nodes)
+    //{
+    //    if (joints.find(node->mName.C_Str()) == joints.end())
+    //        continue;
+
+    //    // skip scaling->rotation->preRotation->translation
+    //    aiNode* parent = node->mParent->mParent->mParent->mParent->mParent;
+
+    //    if (std::string_view(parent->mName.C_Str()) != "RootNode")
+    //        joints[node->mName.C_Str()].parentName = parent->mName.C_Str();
+    //}
+
+
     return 0;
 
     Application application;
