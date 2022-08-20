4b7a4e81536217ce02442c204556cde987527250
Olowerr <oliver.klingspor@hotmail.com>
2022-08-20 19:43:45 +0200

    sad

--- OkayEngine/SkeletalMesh.cpp
+++ OkayEngine/SkeletalMesh.cpp
@@ -12,10 +12,10 @@ namespace Okay
 	{
 		DX11& dx = DX11::Get();
 		
-		dx.CreateVertexBuffer(&vertexBuffer[0], vertices.position.data(), vertices.position.size() * sizeof(Float3));
-		dx.CreateVertexBuffer(&vertexBuffer[1], vertices.uvNormal.data(), vertices.uvNormal.size() * sizeof(UVNormal));
-		dx.CreateVertexBuffer(&vertexBuffer[2], vertices.weights.data(), vertices.weights.size() * sizeof(SkinnedVertex));
-		dx.CreateIndexBuffer(&indexBuffer, vertices.indices.data(), vertices.indices.size() * sizeof(UINT));
+		dx.CreateVertexBuffer(&vertexBuffer[0], vertices.position.data(),UINT( vertices.position.size() * sizeof(Float3)));
+		dx.CreateVertexBuffer(&vertexBuffer[1], vertices.uvNormal.data(),UINT( vertices.uvNormal.size() * sizeof(UVNormal)));
+		dx.CreateVertexBuffer(&vertexBuffer[2], vertices.weights.data(), UINT(vertices.weights.size() * sizeof(SkinnedVertex)));
+		dx.CreateIndexBuffer(&indexBuffer, vertices.indices.data(), UINT(vertices.indices.size() * sizeof(UINT)));
 	}
 
 	SkeletalMesh::~SkeletalMesh()
