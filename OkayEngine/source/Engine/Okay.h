fe517a0bf47f0435bca621573e30e81fc6cc79ac
Olowerr <oliver.klingspor@hotmail.com>
2022-11-05 21:47:12 +0100

    Minor fix

--- OkayEngine/source/Engine/Okay.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Application/Application.h"
-#include "Application/Entity.h"
-#include "Application/Window.h"
-#include "Components/Transform.h"
-#include "Okay/OkayAssert.h"
-#include "Okay/OkayHelper.h"
-#include "Okay/OkayString.h"
