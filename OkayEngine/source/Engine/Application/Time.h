5f3a6b2b6a085842b235bf480119d80e80c71e02
Olowerr <oliver.klingspor@hotmail.com>
2022-12-24 23:32:52 +0100

    Added upTime & Application UpTime

--- OkayEngine/source/Engine/Application/Time.h
+++ OkayEngine/source/Engine/Application/Time.h
@@ -10,31 +10,41 @@ namespace Okay
 
 		static inline float getDT();
 		static inline void setTimeScale(float timeScale);
-		static inline float getInternalDT();
+		static inline float getUpTime();
+
+		static inline float getApplicationUpTime();
+		static inline float getApplicationDT();
 
 		static inline void measure();
 		static inline void start();
 
 	private:
 		static inline std::chrono::time_point<std::chrono::system_clock> frameStart;
-		static inline std::chrono::duration<float> deltaTime;
+		static inline std::chrono::duration<float> dtApp;
+		static inline std::chrono::duration<float> upTime;
+		static inline std::chrono::duration<float> upTimeApp;
 		static inline float timeScale = 1.f;
 	};
 
 	
-	inline float Time::getDT() { return Time::deltaTime.count() * Time::timeScale; }
+	inline float Time::getDT() { return Time::dtApp.count() * Time::timeScale; }
 	inline void Time::setTimeScale(float timeScale) { Time::timeScale = timeScale; }
-	inline float Time::getInternalDT() { return Time::deltaTime.count(); }
+
+	inline float Time::getApplicationDT() { return Time::dtApp.count(); }
+	inline float Time::getUpTime() { return Time::upTime.count(); }
 	
 	inline void Time::measure()
 	{
-		Time::deltaTime = std::chrono::system_clock::now() - Time::frameStart;
+		Time::dtApp = std::chrono::system_clock::now() - Time::frameStart;
 		Time::frameStart = std::chrono::system_clock::now();
+		Time::upTime += Time::dtApp * Time::timeScale;
+		Time::upTimeApp += Time::dtApp;
 	}
 
 	inline void Time::start()
 	{
-		Time::deltaTime = std::chrono::duration<float>(0.f);
+		Time::dtApp = std::chrono::duration<float>(0.f);
+		Time::upTime = std::chrono::duration<float>(0.f);
 		Time::frameStart = std::chrono::system_clock::now();
 	}
 }
