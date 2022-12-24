c471cb6e6abc3c8599191af8137cff4057fd8a42
Olowerr <oliver.klingspor@hotmail.com>
2022-12-24 17:57:29 +0100

    Build event now works

--- OkayEngine/source/Engine/Graphics/Shader.cpp
+++ OkayEngine/source/Engine/Graphics/Shader.cpp
@@ -5,7 +5,7 @@
 
 namespace Okay
 {
-	const std::string Shader::ShaderPath = "../OkayEngine/resources/shaders/bin/";
+	const std::string Shader::ShaderPath = "../OkayEngine/engine_resources/shaders/bin/";
 
 	bool Shader::readShader(std::string_view shaderName, std::string& output)
 	{
