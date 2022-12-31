f89da43ee671be36103d16eb449683953a77ca4f
Olowerr <oliver.klingspor@hotmail.com>
2022-12-31 17:17:10 +0100

    Entity assert if null scene

--- OkayEngine/source/Engine/Application/Entity.h
+++ OkayEngine/source/Engine/Application/Entity.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Scene.h"
+#include "Engine/Okay/Okay.h"
 
 namespace Okay
 {
@@ -14,41 +15,56 @@ namespace Okay
 
 		Entity& operator=(const Entity&) = default;
 
+		// TODO: Move definitions outside of class declartion
+		// and inline those that make sense
+
 		template<typename T, typename... Args>
 		T& addComponent(Args&&... args)
 		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
+			// Assert if has or just repace? or just return it ? // don't just return it
 			return pScene->getRegistry().emplace<T>(entityId, std::forward<Args>(args)...);
 		}
 
 		template<typename... T>
 		bool hasComponent() const
 		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
 			return pScene->getRegistry().all_of<T...>(entityId);
 		}
 
 		template<typename T>
 		T& getComponent()
 		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
+			OKAY_ASSERT(hasComponent<T>(), "Entity doesn't have the given component");
 			return pScene->getRegistry().get<T>(entityId);
 		}
 
 		template<typename T>
 		const T& getComponent() const
 		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
+			OKAY_ASSERT(hasComponent<T>(), "Entity doesn't have the given component");
 			return pScene->getRegistry().get<T>(entityId);
 		}
 
 		template<typename T>
 		bool removeComponent()
 		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
 			return pScene->getRegistry().remove<T>(entityId);
 		}
 
-		operator entt::entity() { return entityId; }
+		operator entt::entity()		  { return entityId; }
 		operator entt::entity() const { return entityId; }
 
 		entt::entity getID() const { return entityId; }
-		bool isValid() const { return entityId != entt::null; }
+		bool isValid() const 
+		{
+			OKAY_ASSERT(pScene, "pScene was nullptr");
+			return pScene->getRegistry().valid(entityId);
+		}
 
 	private:
 		Scene* pScene;
