ba0671f600120b4aaa25a14dffdf47cc1fb87701
Olowerr <oliver.klingspor@hotmail.com>
2023-01-03 13:39:00 +0100

    Material & PointLight display first draft

--- OkayEngine/source/Engine/Components/PointLight.h
+++ OkayEngine/source/Engine/Components/PointLight.h
@@ -7,13 +7,13 @@ namespace Okay
 	struct PointLight
 	{
 		PointLight(const glm::vec3& colour, float intensity, const glm::vec2& attenuation)
-			:colour(colour), intensnity(intensity), attenuation(attenuation)
+			:colour(colour), intensity(intensity), attenuation(attenuation)
 		{ }
 
 		PointLight() = default;
 
 		glm::vec3 colour = glm::vec3(1.f);
-		float intensnity = 1.f;
+		float intensity = 1.f;
 		glm::vec2 attenuation = glm::vec2(0.f, 0.1f); // TODO: Add value description
 	};
 }
