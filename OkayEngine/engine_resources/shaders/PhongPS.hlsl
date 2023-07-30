4e4e130a7e8b9db195678ad5e46fe1602bf240d4
Olowerr <oliver.klingspor@hotmail.com>
2023-07-30 20:06:21 +0200

    Normalized normal in PhongPS.hlsl

--- OkayEngine/engine_resources/shaders/PhongPS.hlsl
+++ OkayEngine/engine_resources/shaders/PhongPS.hlsl
@@ -6,7 +6,8 @@ float4 main(TransformedVertex input) : SV_TARGET
 	const float4 baseColour = diffuseTexture.Sample(simp, uv);
 	const float4 specular = specularTexture.Sample(simp, uv);
 	const float3 posToCam = normalize(camPos - input.worldPos);
-
+    const float3 normal = normalize(input.normal);
+	
 	PointLight poLight;
 	DirectionalLight dirLight;
 
@@ -29,22 +30,22 @@ float4 main(TransformedVertex input) : SV_TARGET
 		
 		const float attu = (1.f / (1.f + poLight.attenuation.x * distance + poLight.attenuation.y * distance * distance));
 		
-		specShading += specular * pow(max(dot(reflect(-posToLight, input.normal), posToCam), 0.f), shinyness) * 
+        specShading += specular * pow(max(dot(reflect(-posToLight, normal), posToCam), 0.f), shinyness) *
 			float4(poLight.colour, 1.f) * poLight.intensity * attu;
 		
-		diffShading += baseColour * max(dot(input.normal, posToLight), 0.f) * float4(poLight.colour, 1.f) * poLight.intensity * attu;
-	}
+        diffShading += baseColour * max(dot(normal, posToLight), 0.f) * float4(poLight.colour, 1.f) * poLight.intensity * attu;
+    }
 
 	// Directional Lights
 	for (i = 0; i < numDir; i++)
 	{
 		dirLight = dirLights[i];
 
-		specShading += specular * pow(max(dot(reflect(dirLight.direction, input.normal), posToCam), 0.f), shinyness) * 
+        specShading += specular * pow(max(dot(reflect(dirLight.direction, normal), posToCam), 0.f), shinyness) *
 			float4(dirLight.colour, 1.f) * dirLight.intensity;
 
-		diffShading += max(dot(input.normal, -dirLight.direction), 0.f) * float4(dirLight.colour, 1.f) * dirLight.intensity;
-	}
+        diffShading += max(dot(normal, -dirLight.direction), 0.f) * float4(dirLight.colour, 1.f) * dirLight.intensity;
+    }
 	
 
 	return saturate(baseColour * (float4(ambientTint, 0.f) * ambientTintIntensity + diffShading + specShading));
